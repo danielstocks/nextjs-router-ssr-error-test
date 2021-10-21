95bef1d3841475d05d3e321d39133f275b8ca59e
Daniel Stocks <daniel@carla.se>
2021-10-21 10:23:43 +0200

    first commit

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Not found</h1>
+    </div>
+  );
+}
---
+++ pages/[id].js
@@ -0,0 +1,51 @@
+import { useRouter } from "next/router";
+
+const pages = {
+  1: {
+    title: "this is page 1",
+  },
+  2: {
+    title: "this is page 2",
+  },
+};
+
+const Page = ({ page }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{page.title}</h1>
+    </div>
+  );
+};
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const page = pages[context.params.id];
+
+  console.log(page);
+
+  if (!page) {
+    return {
+      notFound: true,
+      revalidate: 5,
+    };
+  }
+
+  return {
+    props: { page },
+    revalidate: 5,
+  };
+}
+
+export default Page;
